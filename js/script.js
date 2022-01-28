0685acfd95dc88d423249db2545ed99bce3acbd8
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-01-28 15:23:57 -0300

    header section finished

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ tabsContainer.addEventListener("click", (e)=>{
 document.addEventListener("click",(e)=>{
     if(e.target.classList.contains("view-project-btn")){
         togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
         portfolioItemDetails(e.target.parentElement);
     }
 })
@@ -41,5 +42,46 @@ function portfolioItemDetails(portfolioItem){
     portfolioItem.querySelector(".portfolio-item-title").innerHTML;
 
     document.querySelector(".pp-body").innerHTML=
-    portfolioItem.querySelector(".portfolio-item-detailes").innerHTML;
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
+
+//Toggle navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", ()=>{
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+}
+
+//Active Section
+
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        //Activate the overlay to prevet multiple clicks
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=>{
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        },500)
+    }
+})
+
