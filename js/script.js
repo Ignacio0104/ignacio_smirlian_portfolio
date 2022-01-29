b7bc5e2f1ab2e22d492ae903908659441d223792
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-01-28 21:00:34 -0300

    portfolio template ready

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+/*Page Loader*/
+
+window.addEventListener("load", ()=>{
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-section").classList.add("active");
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector(".page-loader").style.display="none";
+    },600);
+});
+
+
 /*About tabs*/
 
 const tabsContainer = document.querySelector(".about-tabs");
